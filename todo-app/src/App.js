7ea232ebd6f045c568d490b8375c16714d86a044
Sarah Smith <sarahsmithvii@gmail.com>
2023-06-30 19:06:24 +0100

    header created

---
+++ todo-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+import style from './styles/modules/app.module.scss';
+import AppHeader from './components/AppHeader';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
+    </div>
+  );
+}
+
+export default App;
