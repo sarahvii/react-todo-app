06dd7a89f127f09944caaa56dbd8c76377e10b01
Sarah Smith <sarahsmithvii@gmail.com>
2023-07-01 13:01:35 +0100

    task can be captured in state

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
-import style from './styles/modules/app.module.scss';
+import styles from './styles/modules/app.module.scss';
 import AppHeader from './components/AppHeader';
+import AppContent from './components/AppContent';
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
-      <div className={style.app__wrapper}>
+      <div className={styles.app__wrapper}>
         <AppHeader />
+        <AppContent />
       </div>
     </div>
   );
