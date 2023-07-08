9be28ce9d495803c62290f48ea1fff7c96a11681
Sarah Smith <sarahsmithvii@gmail.com>
2023-07-08 22:21:24 +0100

    checkbox functionality added

--- todo-app/src/components/AppContent.js
+++ todo-app/src/components/AppContent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import TodoItem from './TodoItem';
+import styles from '../styles/modules/app.module.scss';
 
 function AppContent() {
   const todoList = useSelector((state) => state.todo.todoList);
   const sortedTodoList = [...todoList];
   sortedTodoList.sort((a, b) => new Date(b.time) - new Date(a.time));
   return (
-    <div>
+    <div className={styles.content__wrapper}>
       {sortedTodoList && sortedTodoList.length > 0
         ? sortedTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
         : 'no todo found'}
--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,16 +1,25 @@
 import { format } from 'date-fns/esm';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-hot-toast';
-import { deleteTodo } from '../slices/todoSlice';
+import { deleteTodo, updateTodo } from '../slices/todoSlice';
 import styles from '../styles/modules/todoitem.module.scss';
 import { getClasses } from '../utils/getClasses';
 import TodoModal from './TodoModal';
+import CheckButton from './CheckButton';
 
 function TodoItem({ todo }) {
   const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
   const [updateModalOpen, setUpdateModalOpen] = useState(false);
+  useEffect(() => {
+    if (todo.status === 'complete') {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [todo.status]);
   const handleDelete = () => {
     dispatch(deleteTodo(todo.id));
     toast.success('Todo Deleted Successfully');
@@ -18,11 +27,20 @@ function TodoItem({ todo }) {
   const handleUpdate = () => {
     setUpdateModalOpen(true);
   };
+  const handleCheck = () => {
+    setChecked(!checked);
+    dispatch(
+      updateTodo({
+        ...todo,
+        status: checked ? 'incomplete' : 'complete',
+      })
+    );
+  };
   return (
     <>
       <div className={styles.item}>
         <div className={styles.todoDetails}>
-          []
+          <CheckButton checked={checked} handleCheck={handleCheck} />
           <div className={styles.text}>
             <p
               className={getClasses([
